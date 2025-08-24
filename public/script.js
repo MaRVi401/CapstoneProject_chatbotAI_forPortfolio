77becf93392f77ea2da952d0f7c2522c3d55fc73
Ahmad Yassin <ahmadyassin4521@gmail.com>
2025-08-24 17:31:21 +0700

    add netlify

--- public/script.js
+++ public/script.js
@@ -29,7 +29,7 @@ chatForm.addEventListener('submit', async (e) => {
     chatInput.value = '';
 
     try {
-        const response = await fetch('/chat', {
+        const response = await fetch('/.netlify/functions/chatbot', {
             method: 'POST',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({ message: userMessage })
